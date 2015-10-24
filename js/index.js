c57c98e8c277ef97a6f266903e21a8b272c2398f
Elliot Cobb <elliot@liveclicker.com>
2015-10-23 21:09:31 -0400

    added ability to autocomplete location inputs

--- js/index.js
+++ js/index.js
@@ -4,95 +4,54 @@
 // selected.
 
 function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -33.8688, lng: 151.2195},
-    zoom: 13
-  });
-  MyLocation();
-  var input = document.getElementById('pac-input');
-
-  var autocomplete = new google.maps.places.Autocomplete(input);
-  autocomplete.bindTo('bounds', map);
-
-  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-
-  var infowindow = new google.maps.InfoWindow();
-  var marker = new google.maps.Marker({
-    map: map
-  });
-  marker.addListener('click', function() {
-    infowindow.open(map, marker);
-  });
-
-  autocomplete.addListener('place_changed', function() {
-    infowindow.close();
-    var place = autocomplete.getPlace();
-    if (!place.geometry) {
-      return;
-    }
-
-    if (place.geometry.viewport) {
-      map.fitBounds(place.geometry.viewport);
-    } else {
-      map.setCenter(place.geometry.location);
-      map.setZoom(17);
-    }
-
-    // Set the position of the marker using the place ID and location.
-    marker.setPlace({
-      placeId: place.place_id,
-      location: place.geometry.location
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -33.8688, lng: 151.2195},
+        zoom: 7
     });
-    marker.setVisible(true);
 
-    drawPolylines(place);
+    initLocationAutoComplete();
 
-    infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
-        'Place ID: ' + place.place_id + '<br>' +
-        place.formatted_address);
-    infowindow.open(map, marker);
-  });
+    var infowindow = new google.maps.InfoWindow();
+    var marker = new google.maps.Marker({
+        map: map
+    });
+    marker.addListener('click', function() {
+        infowindow.open(map, marker);
+    });
+}
+
+function initLocationAutoComplete() {
+    $('.trip-builder-form .location-search').each( function() {
+        var input = $(this)[0];
+        var autoComplete = new google.maps.places.Autocomplete(input);
+    });
 }
 
 function drawPolylines(place)
 {
-        var coordinates = [
-          new google.maps.LatLng(place.latitude, place.longitude),
-          me
-        ]; 
-        var polyline = new google.maps.Polyline({
-          path: coordinates,
-          geodesic: true,
-          strokeColor: '#FF0000',
-          strokeOpacity: 1.0,
-          strokeWeight: 2
-        });
-        polyline.setMap(map);
+    var coordinates = [
+        new google.maps.LatLng(place.latitude, place.longitude),
+        me
+    ];
+    var polyline = new google.maps.Polyline({
+        path: coordinates,
+        geodesic: true,
+        strokeColor: '#FF0000',
+        strokeOpacity: 1.0,
+        strokeWeight: 2
+    });
+    polyline.setMap(map);
 }
 
-function MyLocation()
-{
-        if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition(function(position) {
-                        myLat = position.coords.latitude;
-                        myLng = position.coords.longitude;
-                        renderMap();
-                });
-        }
-        else {
-                alert("Your Browser doesn't support geolocation");
-        }
-}
 function renderMap()
 {
-        me = new google.maps.LatLng(myLat, myLng);
-        map.panTo(me);
-        map.setZoom(6);
-        var image = 'small_blue_ball.png';
-        var marker = new google.maps.Marker({
-                position: me,
-                map: map,
-                icon: image
-        });
+    me = new google.maps.LatLng(myLat, myLng);
+    map.panTo(me);
+    map.setZoom(6);
+    var image = 'small_blue_ball.png';
+    var marker = new google.maps.Marker({
+        position: me,
+        map: map,
+        icon: image
+    });
 }
-
